946f952d1dfb975a8453653d9ee4bd5e5027e34d
Piyushrathoree <meonpath008@gmail.com>
2025-08-03 02:23:12 +0530

    refractor whole packages and make the work again

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,11 +1,9 @@
 import z, { ZodError } from "zod"; // for export zod
-export * from "./src/types";
 export { ZodError, z };
 
-/////
-// packages/common/src/index.ts
-
+///
 import jwt from "jsonwebtoken"; // for export jwt
 const JWT = jwt;
 export { JWT };
 export type { JwtPayload } from "jsonwebtoken";
+////
